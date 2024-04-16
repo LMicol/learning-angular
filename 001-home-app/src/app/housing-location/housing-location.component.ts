aead91ea528903bc6cb18fc81bf6820b8443d1c0
LMicol <lucas.micol.p@gmail.com>
2024-04-16 10:02:46 -0300

    added house-location component and listing on the page

--- 001-home-app/src/app/housing-location/housing-location.component.ts
+++ 001-home-app/src/app/housing-location/housing-location.component.ts
@@ -9,7 +9,7 @@ import { HousingLocation } from '../housing-location';
   template: `
     <section class="listing">
       <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
-      <h2 class="listing-photo"> {{housingLocation.name}} </h2>
+      <h2 class="listing-heading"> {{housingLocation.name}} </h2>
       <p class ="listing-location"> {{housingLocation.city}}, {{housingLocation.name}} </p>
     </section>
   `,
